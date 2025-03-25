32c8c1eeda4d08cb26c2607a634e9265d432e6bd
Cremattsd <110553037+Cremattsd@users.noreply.github.com>
2025-03-25 12:51:55 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@
 echo "Installing Flask & Gunicorn..."
 pip install Flask gunicorn flask-cors
 
-echo "Installing RealNex SDK from private GitHub..."
-pip install "git+https://${GITHUB_TOKEN}@github.com/Cremattsd/realnex-sdk.git@main#egg=real_nex_sync_api_data_facade"
-
-echo "Installation Complete!"
+echo "Installing RealNex SDK from GitHub..."
+pip install "git+https://github.com/Cremattsd/realnex-sdk.git@main#egg=real_nex_sync_api_data_facade"
