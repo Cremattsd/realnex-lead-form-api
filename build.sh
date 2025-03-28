6e8072e98b4b39a3720b7b3ba9656d3d6164f518
Cremattsd <110553037+Cremattsd@users.noreply.github.com>
2025-03-28 11:13:42 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing Flask & Gunicorn..."
-pip install Flask gunicorn flask-cors
-
-echo "Installing SDK from PyPI/Zip via requirements.txt"
-pip install -r requirements.txt
